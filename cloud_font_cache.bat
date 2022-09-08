2d0f25fc956689c74321641e0b9ac4e0a901a288
Brad Arrowood <61139407+bsarrowood@users.noreply.github.com>
2022-09-08 16:31:11 -0500

    Update cloud_font_cache.bat
    added some reference notes/links

--- cloud_font_cache.bat
+++ cloud_font_cache.bat
@@ -3,6 +3,10 @@
 :: Last updated:    2022.09.08
 :: Script name:		cloud_font_cache.bat
 :: Description:		Script to clear the cloud font cache folder
+:: References:
+::                  https://presentyourstory.com/identifying-and-resetting-microsoft-cloud-fonts/
+::                  https://docs.microsoft.com/en-us/typography/fonts/windows_10_font_list#-pan-european-supplemental-fonts
+::                  https://docs.microsoft.com/en-us/typography/font-list/neue-haas-grotesk-text-pro
 
 @ECHO off
 RMDIR "C:\Users\%USERNAME%\AppData\Local\Microsoft\FontCache\4\CloudFonts" /S /Q
